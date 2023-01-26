0340b24411fb8b1dd1d93f1297075bc33644736d
ParLelya <parlelya@vk.com>
2023-01-26 14:06:07 +0300

    Init components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,32 @@
+import { useState, useEffect, useRef } from "react"
+import { ITodo } from "../types/data"
+
 const App: React.FC = () => {
+	const [value, setValue] = useState('');
+	const [todos, setTodos] = useState<ITodo[]>([]);
+	const addTodo = () => {
+		if (value) {
+			setTodos([...todos, {
+				id: Date.now(),
+				title: value,
+				complete: false,
+			}])
+			setValue('')
+		}
+	};
+	//TODO вынести инпут и кнопку в отдельную компоненту
 	return <div>
-		
+		<div>
+			<input
+				value={value}
+				onChange={e => setValue(e.target.value)}
+			/>
+			<button
+				onClick={addTodo}>
+				Добавить
+			</button>
+		</div>
 	</div>
 }
 
-export {App}
+export { App }
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoItem: React.FC = (props) => {
+	return (
+		<div>TodoItem</div>
+	)
+}
+
+export default TodoItem
